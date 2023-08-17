6f99cd6ed8af96bdca0acf080f0c36e669eadf33
Morgana <79321019+morganaschneider@users.noreply.github.com>
2023-08-17 02:25:07 -0300

    Add files via upload

---
+++ tipos-de-dados/script.js
@@ -0,0 +1,24 @@
+// Declare uma variável contendo uma string
+
+var nome = 'Morgana';
+
+// Declare uma variável contendo um número dentro de uma string
+
+var ano = '2023'
+
+// Declare uma variável com a sua idade
+
+var idade = 27;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+
+var sobrenome ='Schneider'
+var nomeCompleto = `${nome} ${sobrenome}`
+
+// Coloque a seguinte frase em uma variável: It's time
+
+var frase = `Its time`;
+
+// Verifique o tipo da variável que contém o seu nome
+var verificarTipoNome = typeof nome;
