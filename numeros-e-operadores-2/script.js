6e826b10e2d4275c20f6bbb305590342a92c2810
Morgana <79321019+morganaschneider@users.noreply.github.com>
2023-08-17 12:26:42 -0300

    Update script.js

--- numeros-e-operadores-2/script.js
+++ numeros-e-operadores-2/script.js
@@ -21,7 +21,7 @@ console.log(x);
 
 // Como dividir o peso por 2?
 
-var numero = '80';
+var numero = '60';
 var unidade = 'kg';
 var peso = numero + unidade; // '80kg'
 var pesoPorDois = peso / 2; // NaN (Not a Number)
