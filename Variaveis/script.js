0161b227fa9ced0b95f7646556360ff401a1400e
Morgana <79321019+morganaschneider@users.noreply.github.com>
2023-08-17 02:10:03 -0300

    Add files via upload

---
+++ Variaveis/script.js
@@ -0,0 +1,24 @@
+// Declarar uma variável com o seu nome
+var nome = 'Morgana';
+
+// Declarar uma variável com a sua idade
+var idade = 27;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+
+var comidaFavorita ='Batata'
+
+// Declarar 5 variáveis diferentes sem valores
+var bot,
+support,
+jungle,
+adc,
+top;
+
+
+console.log(nome, idade, comidaFavorita)
